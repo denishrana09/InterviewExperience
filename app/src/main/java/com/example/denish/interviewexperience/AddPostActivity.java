25d68d5bbe6d5a0d2dc748fac14631c18a7630ef
denish rana <31031186+denishrana09@users.noreply.github.com>
2018-08-13 23:53:37 +0530

    feat: AddPostActivity Layout added

--- app/src/main/java/com/example/denish/interviewexperience/AddPostActivity.java
+++ app/src/main/java/com/example/denish/interviewexperience/AddPostActivity.java
@@ -1,5 +1,7 @@
 package com.example.denish.interviewexperience;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,9 @@ public class AddPostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
+
+        getSupportActionBar().setTitle("Add Experience");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //back arrow in action bar
+//        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#3F51B5")));
     }
 }
