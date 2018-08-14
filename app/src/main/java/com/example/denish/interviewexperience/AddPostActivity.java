4fce6c585661ebdf6f2864859e09de06c6d77a52
denish rana <denishrana09@gmail.com>
2018-08-15 02:12:45 +0530

    fix: Date Format in Post (dd/MM/yyyy)

--- app/src/main/java/com/example/denish/interviewexperience/AddPostActivity.java
+++ app/src/main/java/com/example/denish/interviewexperience/AddPostActivity.java
@@ -8,14 +8,14 @@ import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.denish.interviewexperience.model.Post;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.Calendar;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class AddPostActivity extends AppCompatActivity {
@@ -28,7 +28,8 @@ public class AddPostActivity extends AppCompatActivity {
     AutoCompleteTextView title,company,position;
     EditText description;
     String username,email,userid;
-    Calendar todayDate;
+    DateFormat dateFormat;
+    Date todaysDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,17 +54,18 @@ public class AddPostActivity extends AppCompatActivity {
         description = findViewById(R.id.et_description);
 
         addPostButton = findViewById(R.id.btn_add_post);
+        dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
 
         addPostButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                todayDate = Calendar.getInstance();
-                todayDate.set(Calendar.HOUR_OF_DAY, 0);
+                todaysDate = new Date();
                 //bug : Date not working
                 //feat : check if data already exist for AutoComplete Textview
                 Post model = new Post(title.getText().toString(),company.getText().toString(),
                         position.getText().toString(),
-                        description.getText().toString(),userid,todayDate.getTime().toString(),0);
+                        description.getText().toString(),userid,dateFormat.format(todaysDate),0);
                 mPostsDBRef.push().setValue(model);
 
                 title.setText("");
@@ -73,6 +75,7 @@ public class AddPostActivity extends AppCompatActivity {
                 Toast.makeText(AddPostActivity.this, "Post added succesfully", Toast.LENGTH_SHORT).show();
 
                 startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
             }
         });
 
